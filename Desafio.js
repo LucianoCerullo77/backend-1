d6cd25a1702a60a27ffd7d1bfc0d60e57f078c13
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-06-20 15:30:38 -0300

    se hicieron cambios en la seccion del desafio, el mismo fue completado, su funcionalidad salio existosa

--- Desafio.js
+++ Desafio.js
@@ -7,15 +7,39 @@ class User {
     }
 }
 
-const user = new User('Luciano', 
-'Cerullo',
- ['The Lighthouse', 'The Wall', 'Heartless'], 
- ['Fish', 'Cat', 'Bird']);
 
-getFullName = this.name + ' ' + this.subname;
-addPet = this.pets.push('Dog'); 
-countPets = this.pets.length;
-addBooks = this.books.push('Heartstoppers');
-getBooksNames = this.books.map(book => book);
+const user = {
+    name: 'Luciano',
+    subname: 'Cerullo',
+    books: [{
+        name: 'The Lighthouse',
+        autor: 'J.R.R. Tolkien',
+        year: '1954',
+    },
+    {
+    name : 'The Lord of the Rings',
+        autor: 'J.R.R. Tolkien',
+        year: '1954',
+    }],
+    pets: ['Fish', 'Cat', 'Bird']
+}
+
+
+
+addPets = user.pets.push('Dog');
+getPets = user.pets.join(', ');
+getBooksNames = (user) => {
+    return user.books.map(book => book.name);
+}
+
+const getFullName = (user) => {
+    return `${user.name} ${user.subname}`;
+}
+const countPets = user.pets.length;
+
 
 console.log(user);  // Imprime todo el objeto
+console.log(getFullName(user)); // Imprime el nombre completo
+console.log(getBooksNames(user)); // Imprime el nombre de los libros
+console.log(countPets); // Imprime el numero de mascotas
+console.log(getPets); // Imprime los datos separados por una coma
